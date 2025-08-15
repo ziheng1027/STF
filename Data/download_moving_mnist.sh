4ee358d2a1b4c4d9b46e198f8737dabde5555624
zihengyang <2312279496@qq.com>
2025-08-15 22:00:19 +0800

    添加MovingMNIST数据集下载文件

---
+++ Data/download_moving_mnist.sh
@@ -0,0 +1,8 @@
+mkdir -p MovingMNIST
+cd MovingMNIST
+
+# Download the MovingMNIST dataset
+wget http://www.cs.toronto.edu/~nitish/unsupervised_video/mnist_test_seq.npy
+wget http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
+
+echo "Done!"
