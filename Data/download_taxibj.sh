3cb5bc0f6d3e7c9d9548effd810cb6551ff59e01
zihengyang <2312279496@qq.com>
2025-08-15 22:01:14 +0800

    添加TaxiBJ数据集下载文件

---
+++ Data/download_taxibj.sh
@@ -0,0 +1,8 @@
+mkdir -p TaxiBJ
+cd TaxiBJ
+
+# Download the TaxiBJ dataset
+wget https://github.com/chengtan9907/OpenSTL/releases/download/v0.1.0/taxibj_dataset.zip
+unzip taxibj_dataset.zip
+
+echo "Done!"
