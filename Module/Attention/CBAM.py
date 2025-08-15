35b02ff9fd072dcc0e7311cd4db8d1b1cac1cf11
zihengyang <2312279496@qq.com>
2025-08-15 22:09:05 +0800

    添加CBAM注意力

---
+++ Module/Attention/CBAM.py
@@ -0,0 +1,62 @@
+import torch
+from torch import nn
+
+
+class Flatten(nn.Module):
+    def forward(self, x):
+        return x.view(x.size(0), -1)
+
+
+class ChannelAttention(nn.Module):
+    def __init__(self, in_channels, reduction_ratio=16):
+        super().__init__()
+
+        self.in_channels = in_channels
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+        self.mlp = nn.Sequential(
+            Flatten(),
+            nn.Linear(in_channels, in_channels // reduction_ratio),
+            nn.ReLU(),
+            nn.Linear(in_channels // reduction_ratio, in_channels)
+        )
+    
+    def forward(self, x):
+        x_avg = self.avg_pool(x)
+        x_max = self.max_pool(x)
+        out = self.mlp(x_avg) + self.mlp(x_max)
+        out = torch.sigmoid(out)
+        out = out.unsqueeze(2).unsqueeze(3).expand_as(x)
+        out = x * out
+        return out
+
+
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=7):
+        super().__init__()
+        assert kernel_size in (3, 7), 'kernel size must be 3 or 7'
+        padding = 3 if kernel_size == 7 else 1
+        self.conv = nn.Conv2d(2, 1, kernel_size, padding=padding, bias=False)
+        self.bn = nn.BatchNorm2d(1)
+    
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        out = torch.cat([avg_out, max_out], dim=1)
+        out = self.conv(out)
+        out = self.bn(out)
+        out = x * torch.sigmoid(out)
+        return out
+
+
+class CBAM(nn.Module):
+    def __init__(self, in_channels, reduction_ratio=16, kernel_size=7):
+        super().__init__()
+
+        self.channel_attention = ChannelAttention(in_channels, reduction_ratio)
+        self.spatial_attention = SpatialAttention(kernel_size)
+    
+    def forward(self, x):
+        out = self.channel_attention(x)
+        out = self.spatial_attention(out)
+        return out
