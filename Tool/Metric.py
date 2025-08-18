85050b53b963e72e70c87bada60732c3d2f270d3
zihengyang <2312279496@qq.com>
2025-08-18 18:43:26 +0800

    添加cal_metrics计算指标

--- Tool/Metric.py
+++ Tool/Metric.py
@@ -2,15 +2,24 @@ import torch
 import numpy as np
 
 
-def MSE(target, pred):
+def cal_mse(target, pred):
     return np.mean(np.square(target - pred), axis=(0, 1)).sum()
 
-def MAE(target, pred):
+def cal_mae(target, pred):
     return np.mean(np.abs(target - pred), axis=(0, 1)).sum()
 
-def PSNR(target, pred):
-    mse = MSE(target, pred)
+def cal_psnr(target, pred):
+    mse = cal_mse(target, pred)
     if mse == 0:
         return 100
     max_pixel = 1.0 if target.max() <= 1 else 255
     return 20 * np.log10(max_pixel / np.sqrt(mse))
+
+
+
+def cal_metrics(target, pred):
+    return {
+        "mse": cal_mse(target, pred),
+        "mae": cal_mae(target, pred),
+        "psnr": cal_psnr(target, pred)
+    }
