b72b4dd8a2d9c63619ded03d50e9790b3db809ab
ZihengYang <zihengyang1027@gmail.com>
2025-08-21 17:35:21 +0800

    添加早停机制

---
+++ Tool/EarlyStopping.py
@@ -0,0 +1,22 @@
+class EarlyStopping:
+    def __init__(self, patience=10, delta=1e-5):
+        self.patience = patience
+        self.delta = delta
+        self.best_loss = None
+        self.counter = 0
+    
+    def check(self, val_loss):
+        if self.best_loss is None:
+            self.best_loss = val_loss
+            return False
+        
+        if val_loss < self.best_loss - self.delta:
+            self.best_loss = val_loss
+            self.counter = 0
+            return False
+        else:
+            self.counter += 1
+            if self.counter >= self.patience:
+                return True
+            
+        return False
